798c6ddeaeb1cbf528a27840334a774bd4b6ae76
unknown <rubeenazabin3355@gmail.com>
2023-04-12 12:49:51 +0530

    add demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,19 @@
 package com.example.demo;
-import org.springframework.boot.commandLineRunner;
-import org.apache.logging.log4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.*;
 @SpringBootApplication
-public class DemoApplication implement CommandLineRunner {
-	public static final Logger Logger=LoggerFactory.getLogger(DemoApplication.class,args);
+public class DemoApplication implements CommandLineRunner {
+	public static final Logger Logger= LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
-		Logger.info("its a CI pipeline Job");
+		Logger.info("its a CI pipeline job");
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
-	public void run(String...args) throws Exception{
+
+	@Override
+	public void run(String... args) throws Exception {
 		Logger.info("second log");
 		
 	}
